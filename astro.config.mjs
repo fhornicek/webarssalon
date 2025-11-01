65efe6cb72cb20eacf301680e4096a295a002305
Filip Horníček <filiphornicek@Mac.home>
2025-11-01 18:44:08 +0100

    lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://www.example.com', // zakomentujte nebo vynechte
-  base: '/', // pro root doménu
+site: 'https://www.filiphornicek.cz',
+  base: '/webarssalon', // pro root doménu
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
